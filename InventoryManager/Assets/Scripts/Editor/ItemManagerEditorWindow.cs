fe832edc1f37fbc08f84086ddbac5be52ab4602f
Alec_BigDesktop <alec.b.lisy@gmail.com>
2018-04-24 19:38:37 -0400

    Created Initial shell for editor window.

--- InventoryManager/Assets/Scripts/Editor/ItemManagerEditorWindow.cs
+++ InventoryManager/Assets/Scripts/Editor/ItemManagerEditorWindow.cs
@@ -7,6 +7,27 @@ public class ItemManagerEditorWindow : EditorWindow
 {
     public const string versionNumber = "0.01";
 
+    #region PrivateVariables
+
+    //This is used to cycle through what the right panel should be showing at the top level.
+    enum RightPanelState
+    {
+        createCategory,
+        showItems,
+        createItem
+    }
+    //Current state of the right panel.
+    RightPanelState rightPanelState = RightPanelState.showItems;
+
+    #endregion
+
+    #region PublicVariables
+
+    #endregion
+
+    /// <summary>
+    /// Open the Editor window
+    /// </summary>
     [MenuItem("Item Management/Item Manager " + versionNumber)]
     static void OpenWindow()
     {
@@ -15,9 +36,70 @@ public class ItemManagerEditorWindow : EditorWindow
     }
 
     //this is called every OnGUI frame
-    private void OnGUI()
+    void OnGUI()
     {
-        
+        //Draw the left hand panel.
+        DrawLeftHandCategoryPanel();
+
+        //Cycle through what the Right Hand Panel should be showing at the current time
+        switch (rightPanelState)
+        {
+            //Show Create Category Menu
+            case (RightPanelState.createCategory):
+                DrawRightHandCreateCategoryPanel();
+                break;
+            //Show Items for Current Selected Category
+            case (RightPanelState.showItems):
+                DrawRightHandShowItemsPanel();
+                break;
+            //Show Create Item Menu
+            case (RightPanelState.createItem):
+                DrawRightHandCreateItemsPanel();
+                break;
+            //Show "Please Select or Create a Category" Default Menu
+            default:
+                DrawDefaultMenu();
+                break;
+        }
     }
 
+    /// <summary>
+    /// This Draws the Left Hand Panel that contains the list of current categories and a button to create a new category
+    /// </summary>
+    void DrawLeftHandCategoryPanel()
+    {
+
+    }
+
+    /// <summary>
+    /// This Draws the Right Hand Panel that shows the "Create Category" options
+    /// </summary>
+    void DrawRightHandCreateCategoryPanel()
+    {
+
+    }
+
+    /// <summary>
+    /// This Draws the right hand panel when a category has been selected
+    /// </summary>
+    void DrawRightHandShowItemsPanel()
+    {
+
+    }
+
+    /// <summary>
+    /// This Draws the right hand panel when the user is creating an item.
+    /// </summary>
+    void DrawRightHandCreateItemsPanel()
+    {
+
+    }
+
+    /// <summary>
+    /// This is drawn when nothing is selected
+    /// </summary>
+    void DrawDefaultMenu()
+    {
+
+    }
 }
