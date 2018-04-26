1a706541c6bd12424ca784066a94784e32c697ac
Alec_BigDesktop <alec.b.lisy@gmail.com>
2018-04-25 23:52:39 -0400

    Created ItemCreation script.

---
+++ InventoryManager/Assets/Scripts/Editor/CreateItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This creates a .cs script that inherits from the category that has been selected in the ItemManagerEditorWindow.
+/// This is called by ItemManagerEditorWindow.
+/// </summary>
+public class CreateItem : MonoBehaviour
+{
+
+}
