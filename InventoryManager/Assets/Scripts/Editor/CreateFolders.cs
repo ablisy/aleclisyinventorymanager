e9680f406236a5c9a7596a6784f7b675d98dd0c2
Alec_BigDesktop <alec.b.lisy@gmail.com>
2018-04-25 23:16:26 -0400

    Resources, Items, and Categories folders are now automatically created on load.

---
+++ InventoryManager/Assets/Scripts/Editor/CreateFolders.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This script runs at startup and ensures that all of our 
+/// </summary>
+[InitializeOnLoad]
+public static class CreateFolders
+{
+    static CreateFolders()
+    {
+        if (!AssetDatabase.IsValidFolder("Assets/Resources"))
+        {
+            AssetDatabase.CreateFolder("Assets", "Resources");
+        }
+
+        if (!AssetDatabase.IsValidFolder("Assets/Resources/Categories"))
+        {
+            AssetDatabase.CreateFolder("Assets/Resources", "Categories");
+        }
+
+        if (!AssetDatabase.IsValidFolder("Assets/Resources/Items"))
+        {
+            AssetDatabase.CreateFolder("Assets/Resources", "Items");
+        }
+    }
+}
